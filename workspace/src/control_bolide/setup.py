f72a9d366556d7b04d99fac2b4bcaaef2ba3bf75
Arthasee <baptiste.braun.delvoye@gmail.com>
2025-03-23 15:15:59 +0100

    add entry_points and launch configuration in control_bolide package

--- workspace/src/control_bolide/setup.py
+++ workspace/src/control_bolide/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'control_bolide'
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +23,13 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'ackermann_controller = control_bolide.ackermann_controller:main',
+            'calibration_direction_copy = control_bolide.calibration_direction_copy:main',
+            'calibration_direction = control_bolide.calibration_direction:main',
+            'cmd_vel_vizu = control_bolide.cmd_vel_vizu:main',
+            'obstacle_detection = control_bolide.obstacle_detection:main',
+            'speed_direction_controller = control_bolide.speed_direction_controller:main',
+            'stanley_controller = control_bolide.stanley_controller:main',
         ],
     },
 )
