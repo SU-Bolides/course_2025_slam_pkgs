37de8300a5878722a5cb6c808d0aac9b48a5c84c
Arthasee <baptiste.braun.delvoye@gmail.com>
2025-03-24 17:25:25 +0100

    add teleop_keyboard node in planning_control

---
+++ workspace/src/planning_bolide/planning_bolide/teleop_keyboard.py
@@ -0,0 +1,71 @@
+import threading
+import click
+import rclpy
+from rclpy.node import Node
+from rclpy.executors import ExternalShutdownException
+from std_msgs.msg import Float32
+from bolide_interfaces.msg import SpeedDirection
+
+class KeyboardController(Node):
+    def __init__(self):
+        super().__init__('teleop_node')
+        self.get_logger().info("Teleop node started, Keyboard interrupt (ctrl+c will stop the node)")
+        
+        self.pub = self.create_publisher(SpeedDirection, '/cmd_vel', 10)
+        
+        self.timer = self.Timer(self.Duration(0.4), self.timer_callback)
+        
+        # Define the keycodes
+        self.key_mapping = {'\x1b[A': 'UP', '\x1b[B': 'DOWN',
+                        '\x1b[C': 'RIGHT', '\x1b[D': 'LEFT', 's': 'BRAKE', 'q': 'QUIT'}
+        
+    def timer_callback(self, event):
+        self.publish_speed_direction()
+    
+    def publish_speed_direction(self):
+        self.pub.publish(SpeedDirection(speed=self.current_speed, direction=self.current_direction))
+        
+    def on_key_press(self, in_key):
+        key = self.get_key_char(in_key)
+        if key in self.key_mapping.keys():
+            self.perform_action(self.key_mapping[key])
+        else:
+            self.get_logger.warn(f"[WARN] -- No actions for key : {key}")
+        
+    def get_key_char(self, key):
+        if hasattr(key, 'char'):
+            return key.char
+        else:
+            return key
+    
+    def perform_action(self, action, coeff = 1.0):
+        if action == 'UP':
+            self.current_speed = 1.0 * coeff
+        elif action == 'DOWN':
+            self.current_speed = -1.0 * coeff
+        elif action == 'LEFT':
+            self.current_direction = -1.0 * coeff
+        elif action == 'RIGHT':
+            self.current_direction = 1.0 * coeff
+        elif action == 'BRAKE':
+            self.current_speed = 2.0 * coeff
+        else:
+            self.get_logger().warn(f"Unknown action: {action}")
+        self.publish_speed_direction()
+    
+def main(args=None):
+    rclpy.init(args=args)
+    
+    controller = KeyboardController()
+    thread = threading.Thread(target=rclpy.spin, args=(controller, ), daemon=True)
+    thread.start()
+    
+    try:
+        mykey = click.getchar()
+        char = controller.keycode[mykey]
+        controller.on_key_press(char)
+    except KeyboardInterrupt:
+        controller.get_logger().info("KeyboardInterrupt received. Shutting down...")
+    finally:
+        controller.destroy_node()
+        rclpy.shutdown()
