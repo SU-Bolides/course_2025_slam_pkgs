2740b3de8061dfde7c073d7d8b6c5e82c2d4c0e7
Arthasee <baptiste.braun.delvoye@gmail.com>
2025-03-24 10:19:36 +0100

    add and build sllidar_ros2 package for the lidar

---
+++ workspace/build/sllidar_ros2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/voiture/course_2025_slam_pkgs/workspace/src/sllidar_ros2
+# Build directory: /home/voiture/course_2025_slam_pkgs/workspace/build/sllidar_ros2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
