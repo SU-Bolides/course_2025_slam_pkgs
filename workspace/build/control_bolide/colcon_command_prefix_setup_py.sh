21bf2b74aec1bd41fd7f5afd404c74b7b5d50393
Arthasee <baptiste.braun.delvoye@gmail.com>
2025-03-24 10:13:15 +0100

    add dependecies in setup.py for both packages and build

---
+++ workspace/build/control_bolide/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/voiture/course_2025_slam_pkgs/workspace/install/bolide_interfaces/share/bolide_interfaces/package.sh"
---
+++ workspace/install/control_bolide/share/control_bolide/launch/stanley_launch.launch.py
@@ -0,0 +1,30 @@
+import launch
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='control_bolide',
+            executable='stanley_controller',
+            name='stanley_controller_node',
+            output='screen',
+            respawn=True,
+            parameters=[
+                {'odom_topic': '/pf/pos/odom'},
+                {'waypoints_path': '/home/bolide1/bolide_ws/src/course_2024_pkgs/control_bolide/racelines/esclangon_couloir_reverse.csv'},
+                {'cmd_topic': 'cmd_vel'},
+                {'K_E': 0.4},
+                {'K_H': 0.4},
+                {'K_V': 0.5},
+                {'K_p_obstacle': 0.4},
+                {'K_dh': 0.0},
+                {'velocity_percentage': 1.0},
+                {'steering_limit_deg': 15.3},
+                {'brake_threshold_ms': 5},
+            ]
+        )
+    ])
+
+if __name__ == '__main__':
+    generate_launch_description()
